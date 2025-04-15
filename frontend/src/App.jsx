defa95d9cedc82fac58e67316bb6f1ef3687393c
karakusHalil <karakus_halil@outlook.com>
2025-04-15 21:20:39 +0300

    add campaingSingle component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
+import CampaingSingle from "./components/layout/campaing/CampaingSingle";
 import Footer from "./components/layout/footer/Footer";
 import Header from "./components/layout/header/Header";
+import Policy from "./components/layout/policy/Policy";
 
 function App() {
   return (
     <>
       <Header />
       <h1>E-Commerce</h1>
+      <CampaingSingle />
+      <Policy />
       <Footer />
     </>
   );
