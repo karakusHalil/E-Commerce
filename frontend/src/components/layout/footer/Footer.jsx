ff1cf86e767e9a6c4ee90243292ee56f89f8f032
karakusHalil <karakus_halil@outlook.com>
2025-04-15 17:01:45 +0300

    Fix image src paths

--- frontend/src/components/layout/footer/Footer.jsx
+++ frontend/src/components/layout/footer/Footer.jsx
@@ -46,10 +46,10 @@ export default function Footer() {
                 <div className="footer-contact-bottom">
                   <div className="download-app">
                     <a href="#">
-                      <img src="../../../assets/img/footer/footer/app-store.png" alt />
+                      <img src="img/footer/app-store.png" alt />
                     </a>
                     <a href="#">
-                      <img src="../../../assets/img/footer/google-play.png" alt />
+                      <img src="img/footer/google-play.png" alt />
                     </a>
                   </div>
                   <p className="privacy-text">
@@ -177,7 +177,7 @@ export default function Footer() {
                 </p>
               </div>
               <a href="#">
-                <img src="../../../assets/img/footer/footer/cards.png" alt />
+                <img src="img/footer/cards.png" alt />
               </a>
               <div className="footer-menu">
                 <ul className="footer-menu-list">
