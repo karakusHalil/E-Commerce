e419f082b7b6db46cea88028e94fa8c15202f1b5
karakusHalil <karakus_halil@outlook.com>
2025-04-16 19:37:42 +0300

    add mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const dotenv = require("dotenv");
 
 const app = express();
+const PORT = 5000;
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("MongoDb bağlantısı başarılı...");
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
 app.get("/", (req, res) => {
   res.send("Server JS");
 });
 
-app.listen(5000, () => {
-  console.log("Sunucu çalışıyor...");
+app.listen(PORT, () => {
+  connect();
+  console.log(`Sunucu ${PORT} port üzerinde çalışıyor...`);
 });
