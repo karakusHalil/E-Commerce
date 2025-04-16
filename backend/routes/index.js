8abdd676796e4445b91095a1cfbd7843d4fc769d
karakusHalil <karakus_halil@outlook.com>
2025-04-17 01:49:10 +0300

    Add controller index

---
+++ backend/routes/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+//Routes alıyoruz
+
+const categoryRoute = require("./categories");
+const productRoute = require("./products");
+
+//Route yollarını tanımlıyoruz
+
+router.use("/categories", categoryRoute);
+router.use("/products", productRoute);
