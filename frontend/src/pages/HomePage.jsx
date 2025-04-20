d49be09387ad5c1a9b5178c9fdf0c727005e70c4
karakusHalil <karakus_halil@outlook.com>
2025-04-20 18:20:19 +0300

    Moved blog content to separate Blogs component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import Blogs from "../components/blogs/Blogs";
 import Campaings from "../components/campaings/Campaings";
 import Categories from "../components/categories/Categories";
 import Products from "../components/products/Products";
@@ -267,64 +268,7 @@ const HomePage = () => {
             </div>
           </div>
         </section>
-        <section className="blogs">
-          <div className="container">
-            <div className="section-title">
-              <h2>From Our Blog</h2>
-              <p>Summer Collection New Morden Design</p>
-            </div>
-            <ul className="blog-list">
-              <li className="blog-item">
-                <a href="#" className="blog-image">
-                  <img src="img/blogs/blog1.jpg" alt />
-                </a>
-                <div className="blog-info">
-                  <div className="blog-info-top">
-                    <span>25 Feb, 2021 </span>-<span>0 Comments</span>
-                  </div>
-                  <div className="blog-info-center">
-                    <a href="#">Aliquam hendrerit mi metus</a>
-                  </div>
-                  <div className="blog-info-bottom">
-                    <a href="#">Read More</a>
-                  </div>
-                </div>
-              </li>
-              <li className="blog-item">
-                <a href="#" className="blog-image">
-                  <img src="img/blogs/blog2.jpg" alt />
-                </a>
-                <div className="blog-info">
-                  <div className="blog-info-top">
-                    <span>25 Feb, 2021 </span>-<span>0 Comments</span>
-                  </div>
-                  <div className="blog-info-center">
-                    <a href="#">Aliquam hendrerit mi metus</a>
-                  </div>
-                  <div className="blog-info-bottom">
-                    <a href="#">Read More</a>
-                  </div>
-                </div>
-              </li>
-              <li className="blog-item">
-                <a href="#" className="blog-image">
-                  <img src="img/blogs/blog3.jpg" alt />
-                </a>
-                <div className="blog-info">
-                  <div className="blog-info-top">
-                    <span>25 Feb, 2021 </span>-<span>0 Comments</span>
-                  </div>
-                  <div className="blog-info-center">
-                    <a href="#">Aliquam hendrerit mi metus</a>
-                  </div>
-                  <div className="blog-info-bottom">
-                    <a href="#">Read More</a>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </section>
+        <Blogs />
       </div>
     </>
   );
