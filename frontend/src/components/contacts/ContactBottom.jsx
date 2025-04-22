6c3d618ae6bd1d71dff65a53b8f1c76c9fd04291
karakusHalil <karakus_halil@outlook.com>
2025-04-22 18:33:58 +0300

    Add component ContactBottom for ContactPage

---
+++ frontend/src/components/contacts/ContactBottom.jsx
@@ -0,0 +1,85 @@
+function ContactBottom() {
+  return (
+    <>
+      <div className="contact-bottom">
+        <div className="container">
+          <div className="contact-titles">
+            <h4>Contact with us</h4>
+            <h2>Get In Touch</h2>
+            <p>
+              In hac habitasse platea dictumst. Pellentesque viverra sem nec
+              orci lacinia, in bibendum urna mollis. Quisque nunc lacus, varius
+              vel leo a, pretium lobortis metus. Vivamus consectetur consequat
+              justo.
+            </p>
+          </div>
+          <div className="contact-elements">
+            <form className="contact-form">
+              <div className>
+                <label>
+                  Your Name
+                  <span>*</span>
+                </label>
+                <input type="text" required />
+              </div>
+              <div className>
+                <label>
+                  Your email
+                  <span>*</span>
+                </label>
+                <input type="text" required />
+              </div>
+              <div className>
+                <label>
+                  Subject
+                  <span>*</span>
+                </label>
+                <input type="text" required />
+              </div>
+              <div className>
+                <label>
+                  Your message
+                  <span>*</span>
+                </label>
+                <textarea
+                  id="author"
+                  name="author"
+                  type="text"
+                  value
+                  size={30}
+                  required
+                  defaultValue={""}
+                />
+              </div>
+              <button className="btn btn-sm form-button">Send Message</button>
+            </form>
+            <div className="contact-info">
+              <div className="contact-info-item">
+                <div className="contact-info-texts">
+                  <strong> Clotya Store</strong>
+                  <p className="contact-street">
+                    Clotya Store Germany — 785 15h Street, Office 478/B Green
+                    Mall Berlin, De 81566
+                  </p>
+                  <a href="tel:Phone: +1 1234 567 88">Phone: +1 1234 567 88</a>
+                  <a href="mailto:Email: contact@example.com">
+                    Email: contact@example.com
+                  </a>
+                </div>
+              </div>
+              <div className="contact-info-item">
+                <div className="contact-info-texts">
+                  <strong> Opening Hours</strong>
+                  <p className="contact-date">Monday - Friday : 9am - 5pm</p>
+                  <p>Weekend Closed</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ContactBottom;
