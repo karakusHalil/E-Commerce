4609d12823c74a0a8cf221bca2fa1887a426ed29
karakusHalil <karakus_halil@outlook.com>
2025-04-22 18:15:27 +0300

    add Login and Register components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
       {/* <CardPage /> */}
       {/* <AccountPage /> */}
       {/* <BlogsPage /> */}
-      <HomePage />
+      {/* <HomePage /> */}
       {/* <SingleBlogPage /> */}
       <Brands />
       <CampaingSingle />
--- frontend/src/pages/AccountPage.jsx
+++ frontend/src/pages/AccountPage.jsx
@@ -1,79 +1,14 @@
+import Login from "../components/accounts/Login";
+import Register from "../components/accounts/Register";
+
 const AccountPage = () => {
   return (
     <>
       <section className="account-page">
         <div className="container">
           <div className="account-wrapper">
-            <div className="account-column">
-              <h2>Login</h2>
-              <form>
-                <div>
-                  <label>
-                    <span>
-                      Username or email address{" "}
-                      <span className="required">*</span>
-                    </span>
-                    <input type="text" />
-                  </label>
-                </div>
-                <div>
-                  <label>
-                    <span>
-                      Password <span className="required">*</span>
-                    </span>
-                    <input type="password" />
-                  </label>
-                </div>
-                <p className="remember">
-                  <label>
-                    <input type="checkbox" />
-                    <span>Remember me</span>
-                  </label>
-                  <button className="btn btn-sm">Login</button>
-                </p>
-                <a href="#" className="form-link">
-                  Lost your password?
-                </a>
-              </form>
-            </div>
-            <div className="account-column">
-              <h2>Register</h2>
-              <form>
-                <div>
-                  <label>
-                    <span>
-                      Username <span className="required">*</span>
-                    </span>
-                    <input type="text" />
-                  </label>
-                </div>
-                <div>
-                  <label>
-                    <span>
-                      Email address <span className="required">*</span>
-                    </span>
-                    <input type="email" />
-                  </label>
-                </div>
-                <div>
-                  <label>
-                    <span>
-                      Password <span className="required">*</span>
-                    </span>
-                    <input type="password" />
-                  </label>
-                </div>
-                <div className="privacy-policy-text remember">
-                  <p>
-                    Your personal data will be used to support your experience
-                    throughout this website, to manage access to your account,
-                    and for other purposes described in our{" "}
-                    <a href="#">privacy policy.</a>
-                  </p>
-                  <button className="btn btn-sm">Register</button>
-                </div>
-              </form>
-            </div>
+            <Login />
+            <Register />
           </div>
         </div>
       </section>
