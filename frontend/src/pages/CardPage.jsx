ba62f01ec9a55aba957a26150b851652b9362697
karakusHalil <karakus_halil@outlook.com>
2025-04-22 18:27:49 +0300

    add cardTotal components

--- frontend/src/pages/CardPage.jsx
+++ frontend/src/pages/CardPage.jsx
@@ -1,4 +1,5 @@
 import CardForm from "../components/carts/CardForm";
+import CardTotal from "../components/carts/CardTotal";
 
 function CardPage() {
   return (
@@ -7,46 +8,7 @@ function CardPage() {
         <div className="container">
           <div className="cart-page-wrapper">
             <CardForm />
-            <div className="cart-collaterals">
-              <div className="cart-totals">
-                <h2>Cart totals</h2>
-                <table>
-                  <tbody>
-                    <tr className="cart-subtotal">
-                      <th>Subtotal</th>
-                      <td>
-                        <span id="subtotal">$316.00</span>
-                      </td>
-                    </tr>
-                    <tr>
-                      <th>Shipping</th>
-                      <td>
-                        <ul>
-                          <li>
-                            <label>
-                              Fast Cargo: $15.00
-                              <input type="checkbox" id="fast-cargo" />
-                            </label>
-                          </li>
-                          <li>
-                            <a href="#">Change Address</a>
-                          </li>
-                        </ul>
-                      </td>
-                    </tr>
-                    <tr>
-                      <th>Total</th>
-                      <td>
-                        <strong id="cart-total">$316.00</strong>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-                <div className="checkout">
-                  <button className="btn btn-lg">Proceed to checkout</button>
-                </div>
-              </div>
-            </div>
+            <CardTotal />
           </div>
         </div>
       </section>
