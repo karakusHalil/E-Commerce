667acbfbaad47d837f9d20b0ef5d75ddc3edea8a
karakusHalil <karakus_halil@outlook.com>
2025-04-24 23:38:11 +0300

    fixing

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../models/Product");
 
+//Deneme
+// router.get("/", async (req,res) => {
+//     res.send("Ürün Listesi");
+// })
+
 //Tüm ürünlerin getir getAll()
 router.get("/", async (req, res) => {
   try {
@@ -9,6 +14,17 @@ router.get("/", async (req, res) => {
     res.status(200).json(products);
   } catch (error) {
     res.status(500).json({ error: "Sunucu Hatası !" });
+  }
+});
+
+// Yeni Ürün ekleme
+router.post("/", async (req, res) => {
+  try {
+    const product = new Product(req.body);
+    await product.save();
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({ error: "Sunucu Hatası!" });
   }
 });
 
