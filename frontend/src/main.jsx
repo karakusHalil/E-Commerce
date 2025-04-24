d8fb1c9d1a010f96950587bf85f3809b3f23de6b
karakusHalil <karakus_halil@outlook.com>
2025-04-24 15:39:02 +0300

    Install react-router-dom and add BrowserRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import SiteLayout from "./layouts/SiteLayout.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <SiteLayout>
-      <App />
-    </SiteLayout>
+    <BrowserRouter>
+      <SiteLayout>
+        <App />
+      </SiteLayout>
+    </BrowserRouter>
   </StrictMode>
 );
