575df0600e08cef031a3fb03a1c57d781910a232
karakusHalil <karakus_halil@outlook.com>
2025-04-27 16:48:10 +0300

    Admin page privileges added

--- frontend/src/layouts/AdminLayout.jsx
+++ frontend/src/layouts/AdminLayout.jsx
@@ -1,17 +1,73 @@
-import { Layout } from "antd";
+import { Layout, Menu } from "antd";
+import { useNavigate } from "react-router-dom";
+import {
+  AppstoreOutlined,
+  ShoppingOutlined,
+  DashboardOutlined,
+  UserOutlined,
+  TagsOutlined,
+} from "@ant-design/icons";
 const { Header, Footer, Sider, Content } = Layout;
 
 function AdminLayout() {
+  const navigate = useNavigate();
+  const items = [
+    {
+      key: "1",
+      icon: <DashboardOutlined />,
+      label: "DashBoard",
+      path: "/admin",
+      onClick: () => {
+        navigate("/admin");
+      },
+    },
+    {
+      key: "2",
+      icon: <AppstoreOutlined />,
+      label: "Category",
+      path: "/admin/category",
+      onClick: () => {
+        navigate("/admin/category");
+      },
+    },
+    {
+      key: "3",
+      icon: <ShoppingOutlined />,
+      label: "Product",
+      path: "/admin/product",
+      onClick: () => {
+        navigate("/admin/product");
+      },
+    },
+    {
+      key: "4",
+      icon: <UserOutlined />,
+      label: "User",
+      path: "/admin/user",
+      onClick: () => {
+        navigate("/admin/user");
+      },
+    },
+    {
+      key: "5",
+      icon: <TagsOutlined />,
+      label: "Coupon",
+      path: "/admin/coupon",
+      onClick: () => {
+        navigate("/admin/coupon");
+      },
+    },
+  ];
   return (
     <>
       <div className="admin-layout">
         <Layout style={{ minHeight: "100vh" }}>
-          <Sider width="25%" style={{ color: "white" }}>
-            Sider
+          <Sider width="20%" style={{ color: "white" }}>
+            <Menu mode="inline" theme="dark" items={items} />
           </Sider>
           <Layout>
             <Header style={{ color: "white" }}>Header</Header>
-            <Content style={{}}>Content</Content>
+            <Content style={{ padding: "30px" }}>Content</Content>
             <Footer style={{}}>Footer</Footer>
           </Layout>
         </Layout>
