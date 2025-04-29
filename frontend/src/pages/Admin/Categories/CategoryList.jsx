96095f5b9ed0038870eac55b295847307ac24756
karakusHalil <karakus_halil@outlook.com>
2025-04-29 10:58:27 +0300

    add delete categories

--- frontend/src/pages/Admin/Categories/CategoryList.jsx
+++ frontend/src/pages/Admin/Categories/CategoryList.jsx
@@ -1,8 +1,57 @@
-import { Table } from "antd";
+import { Table, message, Button } from "antd";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function CategoryList() {
   const [dataList, setDataList] = useState([]);
+  const navigate = useNavigate();
+
+  const columns = [
+    {
+      title: "Image",
+      dataIndex: "img",
+      width: "25%",
+      render: (img, record) => (
+        <img alt={`/${record.img}`} src={`/${record.img}`} />
+      ),
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      width: "%50",
+      align: "center",
+      sorter: (a, b) => a.name.localeCompare(b.name),
+    },
+    {
+      title: "Process",
+      key: "process",
+      width: "25%",
+      render: (record) => (
+        <>
+          <Button
+            color="cyan"
+            variant="solid"
+            onClick={() => navigate(`/admin/categories/update/${record._id}`)}
+            style={{ marginRight: "5px" }}
+          >
+            Update
+          </Button>
+          <Button
+            color="danger"
+            variant="solid"
+            onClick={() => deleteCategories(record._id)}
+          >
+            Delete
+          </Button>
+        </>
+      ),
+    },
+  ];
+
+  const onChange = (pagination, filters, sorter, extra) => {
+    console.log("params", pagination, filters, sorter, extra);
+  };
 
   const getCategories = async () => {
     try {
@@ -18,56 +67,31 @@ function CategoryList() {
     }
   };
 
+  const deleteCategories = async (categoryId) => {
+    try {
+      const response = await fetch(
+        `http://localhost:5000/api/categories/${categoryId}`,
+        {
+          method: "DELETE",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ _id: categoryId }),
+        }
+      );
+      if (response.ok) {
+        message.success("Kategori başarıyla silindi.");
+        await getCategories();
+        navigate("/admin/categories/list");
+      } else {
+        message.error("Kategori silme işlemi başarısız !");
+      }
+    } catch (error) {
+      console.log("Sunucu Hatası !", error);
+    }
+  };
+
   useEffect(() => {
     getCategories();
-    console.log(dataList);
   }, []);
-
-  const columns = [
-    {
-      title: "ID",
-      dataIndex: "_id",
-    },
-    {
-      title: "Name",
-      dataIndex: "name",
-      sorter: (a, b) => a.name.localeCompare(b.name),
-    },
-    {
-      title: "Image",
-      dataIndex: "img",
-      width: "40%",
-    },
-  ];
-  //   const data = [
-  //     {
-  //       key: "1",
-  //       name: "John Brown",
-  //       age: 32,
-  //       address: "New York No. 1 Lake Park",
-  //     },
-  //     {
-  //       key: "2",
-  //       name: "Jim Green",
-  //       age: 42,
-  //       address: "London No. 1 Lake Park",
-  //     },
-  //     {
-  //       key: "3",
-  //       name: "Joe Black",
-  //       age: 32,
-  //       address: "Sydney No. 1 Lake Park",
-  //     },
-  //     {
-  //       key: "4",
-  //       name: "Jim Red",
-  //       age: 32,
-  //       address: "London No. 2 Lake Park",
-  //     },
-  //   ];
-  const onChange = (pagination, filters, sorter, extra) => {
-    console.log("params", pagination, filters, sorter, extra);
-  };
   return (
     <>
       <Table columns={columns} dataSource={dataList} onChange={onChange} />
