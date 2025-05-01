3cc20dbb9d2cfae63c3e83e516e17fb7dd1a1e96
karakusHalil <karakus_halil@outlook.com>
2025-05-01 18:40:40 +0300

    Add create product page

---
+++ frontend/src/pages/Admin/Products/CreateProduct.jsx
@@ -0,0 +1,30 @@
+import { Button, Form, Input } from "antd";
+
+function CreateProduct() {
+  const [form] = Form.useForm();
+  const formLayout = "vertical";
+  return (
+    <>
+      <Form
+        layout={formLayout}
+        form={form}
+        initialValues={{ layout: formLayout }}
+        onFinish={{}}
+      >
+        <Form.Item label="Category Name" name="name">
+          <Input placeholder="Category name enter..." />
+        </Form.Item>
+        <Form.Item label="Category Image" name="img">
+          <Input placeholder="Category Image enter..." />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Create
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
+}
+
+export default CreateProduct;
