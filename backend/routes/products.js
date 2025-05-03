d074b893018f32afe8f34a77767cedeb4245f998
karakusHalil <karakus_halil@outlook.com>
2025-05-03 23:26:40 +0300

    add uploads images post method

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Product = require("../models/Product");
+const multer = require("multer"); // Dosya yükleme ayarı
 
 //Deneme
 // router.get("/", async (req,res) => {
@@ -42,7 +43,6 @@ router.post("/", async (req, res) => {
   }
 });
 
-//Id bilgisine göre ürün getirme
 router.put("/:productId", async (req, res) => {
   try {
     const productId = req.params.productId;
@@ -74,6 +74,47 @@ router.delete("/:productId", async (req, res) => {
     res.status(200).json(deletedProduct);
   } catch (error) {
     res.status(500).json({ error: "Sunucu Hatası !" });
+  }
+});
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    // uploads/img/products/ dizinine yükleme
+    const productDir = path.join(__dirname, '../uploads/img/products/product1'); 
+    cb(null, productDir); 
+  },
+  filename: (req, file, cb) => {
+    // Benzersiz dosya adı oluşturma
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, uniqueSuffix + path.extname(file.originalname));
+  }
+});
+
+// Multer upload ayarları
+const upload = multer({ storage });
+
+// Ürün resmini yükleme ve ürün verisini güncelleme
+router.put('/:id/image', upload.array('image', 3), async (req, res) => {
+  try {
+    // Yüklenen dosyaların yolu
+    const imagePaths = req.files.map(file => `/img/products/product1/${file.filename}`);
+
+    // Ürün verisini güncelleme
+    const product = await Product.findByIdAndUpdate(
+      req.params.id, 
+      { $set: { images: imagePaths } },
+      { new: true }
+    );
+
+    if (!product) {
+      return res.status(404).json({ message: 'Ürün bulunamadı' });
+    }
+
+    // Ürünle birlikte güncellenmiş resimleri döndür
+    res.status(200).json(product);
+  } catch (error) {
+    console.error("Hata:", error);
+    res.status(500).send("Fotoğraf yüklenemedi");
   }
 });
 
