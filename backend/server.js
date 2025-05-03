aeec4791f52e75d1acfb1baedbc5aaf8f50be291
karakusHalil <karakus_halil@outlook.com>
2025-05-03 23:25:45 +0300

    add path

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = express();
-// const cors = require("cors");
+const cors = require("cors");
 const mainRoute = require("./routes/index");
+const path = require("path");
 
 const PORT = 5000;
 dotenv.config();
 
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 app.use("/api", mainRoute);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const connect = async () => {
   try {
